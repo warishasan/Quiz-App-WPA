bfa82d2d756fce03d0c8131151a68132a9eb02b0
warishasan <waris.hasan00@gmail.com>
2020-08-07 21:06:02 +1200

    improved service worker to dynamically fetch cache for offline use

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -20,19 +20,18 @@ const cacheAssets = [
 
 
 const cacheAssets = [
- './static/css/main.5054d6d7.chunk.css',
- './static/js/2.f6a9a0d2.chunk.js',
- './static/js/main.6eb23ee3.chunk.js',
- './index.html',
- '/'
- 
-];
+  './',
+  './static/css/main.5054d6d7.chunk.css',
+  './static/js/2.f6a9a0d2.chunk.js',
+  './static/js/main.6eb23ee3.chunk.js',
+  './index.html'
+  
+  
+ ];
 
 
 
-
-// Call Install Event
-self.addEventListener('install', e => {
+ self.addEventListener('install', e => {
   console.log('Service Worker: Installed');
 
   e.waitUntil(
@@ -47,47 +46,64 @@ self.addEventListener('install', e => {
 });
 
 
-this.addEventListener("fetch", (event) => {
 
-  if (!navigator.onLine){
-    event.respondWith(
-      caches.match(event.request).then((resp) => {
-  
-        if (resp) {
-          return resp
-        }
 
-        let requestUrl = event.request.clone();
-        fetch(requestUrl)
+
+ self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
       }
-  
-      )
-  
-    )
-  
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
 
-  }
- 
-})
+      .then(response => {
+        
+        return caches.open(cacheName).then(cache => {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      });
+
+    }).catch(error => {
+
+        console.log("error in loading pages")
+
+    })
+  );
+});
 
 
-// Call Activate Event
-self.addEventListener('activate', e => {
-  console.log('Service Worker: Activated');
-  // Remove unwanted caches
-  e.waitUntil(
+
+
+
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheAllowlist = [cacheName];
+
+  event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== cacheName) {
-            console.log('Service Worker: Clearing Old Cache');
-            return caches.delete(cache);
+        cacheNames.map(cacheName => {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
           }
         })
       );
     })
   );
 });
+
+
+
+
+
+
 
 
 
